9daf7c7778e178960d053c30c3953b54bfc8a726
Tuan Ngo <anhtuanngo101@gmail.com>
2021-02-08 05:38:07 -0600

    add pdf viewer

--- src/Pages/Library.jsx
+++ src/Pages/Library.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-
+import PDFdisplay from "./PDFdisplay";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 class Library extends Component {
   state = {};
   render() {
-    return <h1>Library Page</h1>;
+    return (
+      <Button variant="primary">
+        <Link to="/pdfview">Click here</Link>
+      </Button>
+    );
   }
 }
 
