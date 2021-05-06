1bf49a9f779ab7ef0b3ae134a216a1be0a4956b6
Peter <ducanh199818@gmail.com>
2021-05-06 04:11:56 -0500

    set files up to run on Heroku

--- server/app.js
+++ server/app.js
@@ -64,6 +64,11 @@ passport.use(new GoogleStrategy({
 app.use("/", userRouter);
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started");
 });
